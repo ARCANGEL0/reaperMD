50859ada28eda810f25436465bb6caadfb0c880b
Henry <henry.arcangello@proton.me>
2025-01-10 23:49:42 +0000

    update plugins/dono-update.js

--- plugins/dono-update.js
+++ plugins/dono-update.js
@@ -22,9 +22,7 @@ let stdout = execSync('git pull origin master' + (m.fromMe && text ? ' ' + text 
 
     const fetch = `┌──[ Ǥнѳᔕт ]─[/usr/bin]
 └─ git pull origin master
-> . . . . . . . . . . . 
-${formattedStdout}
-> . . . . . . . . . . .`;
+${formattedStdout}`;
 ////
 
 
