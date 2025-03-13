ba5ec13fc37b6060e6b63dc90b1f8adaf340f49e
Henry <henry.arcangello@proton.me>
2025-03-13 22:00:41 +0000

    update plugins/grupoInventario.js

--- plugins/grupoInventario.js
+++ plugins/grupoInventario.js
@@ -223,7 +223,7 @@ async function generateInventoryImage() {
     registerFont('fonts/DutchbrigadeRegular-8M7VJ.otf', { family: 'DutchbrigadeRegular-8M7VJ' });
     registerFont('fonts/WoodgodRegular-3zpjG.ttf', { family: 'WoodgodRegular-3zpjG' });
     
-    let canvasWidth = 1280
+    let canvasWidth = 2048
     let canvasHeight =720
     const canvas = createCanvas(canvasWidth,canvasHeight);  // Assuming image size based on description
     const ctx = canvas.getContext('2d');
