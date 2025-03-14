a201fd04f377e15b054fd2f2b2c1c68083592663
Henry <henry.arcangello@proton.me>
2025-03-14 21:19:51 +0000

    update plugins/grupoInventario.js

--- plugins/grupoInventario.js
+++ plugins/grupoInventario.js
@@ -239,8 +239,8 @@ const profileImagePath = await conn.profilePictureUrl(m.sender, 'image').catch((
 const profileImage = await loadImage(profileImagePath);
 const profileWidth = 250;  // Largura do quadrado central
 const profileHeight = 195; // Altura do quadrado central
-const profileX = 898;  // Posição X do quadrado
-const profileY = 630; // Posição Y do quadrado
+const profileX = 900;  // Posição X do quadrado
+const profileY = 610; // Posição Y do quadrado
 
 
 ctx.save();  
