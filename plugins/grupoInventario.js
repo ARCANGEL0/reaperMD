b75239d09590b0b800d7d926683a45218a790644
Henry <henry.arcangello@proton.me>
2025-03-14 15:37:05 +0000

    update plugins/grupoInventario.js

--- plugins/grupoInventario.js
+++ plugins/grupoInventario.js
@@ -246,7 +246,7 @@ const profileY = 566; // Posição Y do quadrado
 ctx.save();  
 ctx.beginPath();  
 // Começa na base esquerda
-ctx.moveTo(profileX, profileY + profileHeight);
+ctx.moveTo(profileX +20, profileY + profileHeight);
 
 // Primeiro degrau: sobe um pouco
 ctx.lineTo(profileX-70, profileY + profileHeight - 30);
